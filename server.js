c23d18fa98cb1c6e5ef00d087c154555ee6e60ed
Noah Burton <nburton115@gmail.com>
2023-09-19 15:38:13 -0600

    express-handlebars server.js '

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({})
 
 //express
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+//TODO: add controller
+//app.use(require('./controllers/'))
 
 app.listen(PORT, () => {
     console.log('the server is running at localhost:' + PORT);
