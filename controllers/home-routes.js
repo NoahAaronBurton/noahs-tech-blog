2dbb7a267623a55ee45c3a84e01ba6b7559f5471
Noah Burton <nburton115@gmail.com>
2023-09-20 13:02:40 -0600

    to do comments for next branch

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 // todo: link models
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { // todo: serialize data
+
+     // We use map() to iterate over dishData and then add .get({ plain: true }) each object to serialize it. 
+     //const dishes = dishData.map((dish) => dish.get({ plain: true }));
+     // We render the template, 'all', passing in dishes, a new array of serialized objects.
+     //res.render('all', { dishes });
     res.render('home');
 });
 
